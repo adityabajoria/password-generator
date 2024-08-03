05d83f6affc4f0a92a64616903e219c06e92a863
Aditya Bajoria <aadityaudel.edu>
2024-08-03 10:15:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <p>Password Generator</p>
+    <div className="container">
+      {/* Password and Copy*/}
+      <div className="header">
+        <div className="text">iwnde320349</div>
+        <button className="copy-btn">Copy</button>
+        {/* Character Length */}
+        {/* Checkboxes */}
+        {/* Strength */}
+        {/* Error Handling */}
+        {/* Generate Password */}
+      </div>
     </div>
   );
 }
